8038fbbb979bb8b6179da31c8be679f526e507bc
allwintitus <152678333+allwintitus17@users.noreply.github.com>
2024-11-20 15:22:08 +0530

    Update noteService.js

--- frontend/src/features/notes/noteService.js
+++ frontend/src/features/notes/noteService.js
@@ -30,6 +30,7 @@
 import axios from 'axios';
 
 const API_URL = '/api/tickets/';
+axios.defaults.baseURL='https://ticketing-system-fullstack-project.onrender.com'
 
 const getNotes = async (ticketId, token) => {
     const config = {
