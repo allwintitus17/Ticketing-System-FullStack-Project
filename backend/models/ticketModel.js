3d855b5b8bebc09adef98147c9f95f31b7b40050
allwintitus <71762131006@cit.edu.in>
2024-11-21 15:46:46 +0530

    new Changes

--- backend/models/ticketModel.js
+++ backend/models/ticketModel.js
@@ -1,30 +1,3 @@
-// const mongoose=require('mongoose')
-
-// const ticketSchema=mongoose.Schema({
-//     user:{
-//         type:mongoose.Schema.Types.ObjectId,
-//         required:true,
-//         ref: 'User'
-//     },product:{
-//         type:String,
-//         required:[true,'Please please select an Product'],
-//         enum:['iPhone','Macbook Pro','iMac','iPad'],
-//     },description:{
-//         type:String,
-//         required:[true,'please enter a description of the issue'],
-//     },
-//     status:{
-//         type:Boolean,
-//         required:true,
-//         enum:['new','open','closed'],
-//         default:'new',
-//     }
-
-// },{
-//     timestamps:true,
-// })
-
-// module.exports=mongoose.model('Ticket',ticketSchema)
 
 const mongoose = require('mongoose');
 
